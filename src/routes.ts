3796f819adcb7a80641c656da928470618115442
Gustavo Moreira <gustavomoreira88@gmail.com>
2022-04-01 18:26:39 -0300

    feat: Delivery search without deliveryman attached

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import { AuthenticateClientController } from './modules/account/authenticateClie
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController';
+import { FindAllAvailableController } from './modules/deliveries/useCases/findAllAvailable/FindAllAvailableController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 
 const routes = Router();
@@ -15,6 +16,7 @@ const authenticateDeliverymanController =
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 const createDeliveryController = new CreateDeliveryController();
+const findAllAvailableController = new FindAllAvailableController();
 
 routes.post('/client/authenticate', authenticateClientController.handle);
 routes.post(
@@ -30,5 +32,6 @@ routes.post(
   ensureAuthenticateClient,
   createDeliveryController.handle,
 );
+routes.get('/delivery/available', findAllAvailableController.handle);
 
 export { routes };
